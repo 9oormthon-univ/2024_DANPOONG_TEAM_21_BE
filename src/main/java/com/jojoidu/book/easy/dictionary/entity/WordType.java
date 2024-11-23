49acab576a7f632db38fdfa9e83c008510cb66bf
hawardShin <joushin@student.42seoul.kr>
2024-11-23 22:41:35 +0900

    Feat: Set Word Domain

--- src/main/java/com/jojoidu/book/easy/dictionary/entity/WordType.java
+++ src/main/java/com/jojoidu/book/easy/dictionary/entity/WordType.java
@@ -1,10 +1,11 @@
 package com.jojoidu.book.easy.dictionary.entity;
 
+import com.jojoidu.book.easy.dictionary.dto.WordReqType;
+
 import lombok.Getter;
 
 @Getter
 public enum WordType {
-	ALL("ALL", "전체"),
 	CAFE("CAFE", "카페"),
 	FOOD("FOOD", "음식");
 
@@ -14,6 +15,20 @@ public enum WordType {
 	WordType(String key, String title) {
 		this.key = key;
 		this.title = title;
+	}
+
+	public static WordType from(WordReqType wordReqType) {
+		if (wordReqType == null) {
+			return null;
+		}
+
+		for (WordType wordType : WordType.values()) {
+			if (wordType.getKey().equals(wordReqType.getKey())) {
+				return wordType;
+			}
+		}
+
+		throw new IllegalArgumentException("WordReqType에 매핑되는 WordType이 없습니다: " + wordReqType);
 	}
 
 }
