8ae61ef83555700ef2a42f88fccaef0b4366665d
hawardShin <joushin@student.42seoul.kr>
2024-11-23 16:39:30 +0900

    Feat: Set Global Error config

---
+++ src/main/java/com/jojoidu/book/easy/global/exception/GlobalException.java
@@ -0,0 +1,7 @@
+package com.jojoidu.book.easy.global.exception;
+
+public class GlobalException extends GlobalCodeException {
+	public GlobalException(GlobalErrorCode errorCode) {
+		super(errorCode);
+	}
+}
