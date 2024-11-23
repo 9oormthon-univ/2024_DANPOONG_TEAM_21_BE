1bd3cd9d6f68f68a0af255666250e393f85398a8
orieasy1 <easy1nhard2@gmail.com>
2024-11-23 17:14:07 +0900

    Feat: 학습 영상 엔티티 추가

---
+++ src/main/java/com/jojoidu/book/easy/tutorials/entity/Video.java
@@ -0,0 +1,25 @@
+package com.jojoidu.book.easy.tutorials.entity;
+
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Video {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "video_url", nullable = false)
+    private String videoUrl;
+
+    @Builder
+    public Video(String videoUrl) {
+        this.videoUrl = videoUrl;
+    }
+}
